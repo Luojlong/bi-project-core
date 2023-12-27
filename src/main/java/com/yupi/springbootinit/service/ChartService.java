7e049ad2d9cd27a163d4b2c224458ae48f5931c8
jlongluo <993672800@qq.com>
2023-12-27 19:22:58 +0800

    后端初始化

---
+++ src/main/java/com/yupi/springbootinit/service/ChartService.java
@@ -0,0 +1,13 @@
+package com.yupi.springbootinit.service;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 99367
+* @description 针对表【chart(图表信息表)】的数据库操作Service
+* @createDate 2023-12-25 16:07:42
+*/
+public interface ChartService extends IService<Chart> {
+
+}
